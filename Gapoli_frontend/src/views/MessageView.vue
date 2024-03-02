99207160af4ace1c86606b84a2a6aafd9f45bd10
AlirezaCE <alirezace79@gmail.com>
2024-03-02 20:21:50 +0330

    add message page

---
+++ Gapoli_frontend/src/views/MessageView.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+        <div class="main-left col-span-1">
+            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                <div class="space-y-4">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <img src="https://img.favpng.com/8/7/15/hulk-superhero-icon-png-favpng-j7ZaifhXrReBKUiFaaMYQ22JJ.jpg" class="rounded-full w-[40px]">
+                            <p class="text-xs"><strong>Hulk Smash</strong></p>
+                        </div>
+                        <span class="text-xs text-gray-500">18 minutes ago</span>
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <img src="https://img.favpng.com/8/7/15/hulk-superhero-icon-png-favpng-j7ZaifhXrReBKUiFaaMYQ22JJ.jpg" class="rounded-full w-[40px]">
+                            <p class="text-xs"><strong>Hulk Smash</strong></p>
+                        </div>
+                        <span class="text-xs text-gray-500">18 minutes ago</span>
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <img src="https://img.favpng.com/8/7/15/hulk-superhero-icon-png-favpng-j7ZaifhXrReBKUiFaaMYQ22JJ.jpg" class="rounded-full w-[40px]">
+                            <p class="text-xs"><strong>Hulk Smash</strong></p>
+                        </div>
+                        <span class="text-xs text-gray-500">18 minutes ago</span>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        <div class="main-center col-span-3 space-y-4">
+
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <div class="flex flex-col flex-grow p-4">
+                    <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                        <div>
+                            <div class="bg-blue-600 text-white p-4 rounded-l-lg rounded-br-lg">
+                                <p class="text-sm">
+                                    Hey... <br>
+                                    How you doing to day?
+                                </p>
+                            </div>
+                            <span class="text-xs text-gray-500 leading-none">2 minutes ago</span>
+                        </div>
+                        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+                            <img src="https://img.favpng.com/8/7/15/hulk-superhero-icon-png-favpng-j7ZaifhXrReBKUiFaaMYQ22JJ.jpg" class="rounded-full w-[40px]">
+                        </div>
+                    </div>
+
+                    <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                        <div>
+                            <div class="bg-blue-600 text-white p-4 rounded-l-lg rounded-br-lg">
+                                <p class="text-sm">
+                                    the strongest avenger is here........... I am always looking for things to smash
+                                </p>
+                            </div>
+                            <span class="text-xs text-gray-500 leading-none">2 minutes ago</span>
+                        </div>
+                        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+                            <img src="https://img.favpng.com/8/7/15/hulk-superhero-icon-png-favpng-j7ZaifhXrReBKUiFaaMYQ22JJ.jpg" class="rounded-full w-[40px]">
+                        </div>
+                    </div>
+
+                    <div class="flex w-full mt-2 space-x-3 max-w-md">
+                        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+                            <img src="https://img.favpng.com/8/7/15/hulk-superhero-icon-png-favpng-j7ZaifhXrReBKUiFaaMYQ22JJ.jpg" class="rounded-full w-[40px]">
+                        </div>
+                        <div>
+                            <div class="bg-purple-400 p-4 rounded-r-lg rounded-bl-lg">
+                                <p class="text-sm">
+                                    the strongest avenger is here........... I am always looking for things to smash
+                                </p>
+                            </div>
+                            <span class="text-xs text-gray-500 leading-none">2 minutes ago</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="p-2 bg-white border border-gray-200 rounded-lg">
+                <div class="p-4 max-w-7xl mx-auto grid grid-cols-10 gap-4">
+                    <div class="col-span-9">
+                        <textarea class="p-4 w-full bg-gray-100 rounded-lg" placeholder="Message"></textarea>
+                    </div>
+                    <div class="col-span-1 flex items-center justify-center">
+                        <a href="#" class="inline-block py-2 px-2 bg-purple-500 text-white rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
