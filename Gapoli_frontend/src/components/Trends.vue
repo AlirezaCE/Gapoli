a47badfa1690aef75ada222ea41d36ba9c2c9d9d
AlirezaCE <alirezace79@gmail.com>
2024-03-03 16:47:35 +0330

    add trends components

---
+++ Gapoli_frontend/src/components/Trends.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">Trends</h3>
+        <div class="space-y-4">
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>Avengers</strong><br>
+                    <span class="text-gray-500">990 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-800 text-xs text-white rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>Avengers</strong><br>
+                    <span class="text-gray-500">990 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-800 text-xs text-white rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>Avengers</strong><br>
+                    <span class="text-gray-500">990 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-800 text-xs text-white rounded-lg">Explore</a>
+            </div>
+        </div>
+    </div>
+</template>
--- Gapoli_frontend/src/views/SearchView.vue
+++ Gapoli_frontend/src/views/SearchView.vue
@@ -75,49 +75,21 @@
 
         <div class="main-right col-span-1 space-y-4">
             <PeopleYouMayKnow />
-            
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-xl">Trends</h3>
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>Avengers</strong><br>
-                            <span class="text-gray-500">990 posts</span>
-                        </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-800 text-xs text-white rounded-lg">Explore</a>
-                    </div>
-                
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>Avengers</strong><br>
-                            <span class="text-gray-500">990 posts</span>
-                        </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-800 text-xs text-white rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>Avengers</strong><br>
-                            <span class="text-gray-500">990 posts</span>
-                        </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-800 text-xs text-white rounded-lg">Explore</a>
-                    </div>
-                </div>
-            </div>
+            <Trends />
         </div>
+        
     </div>
 </template>
 
 <script>
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
+import Trends from '../components/Trends.vue'
 
 export default{
     name: 'SearchView',
     components: {
         PeopleYouMayKnow,
+        Trends,
     }
 }
 </script>
