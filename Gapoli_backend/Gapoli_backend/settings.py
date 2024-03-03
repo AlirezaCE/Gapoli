2419d9eb9c406779009fb06d78570be8291365f7
AlirezaCE <alirezace79@gmail.com>
2024-03-04 00:56:27 +0330

    add user model

--- Gapoli_backend/Gapoli_backend/settings.py
+++ Gapoli_backend/Gapoli_backend/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
+    "account",
 ] 
 
 MIDDLEWARE = [
@@ -147,3 +148,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'account.User'
