f9bf20b4468aed70015f09cfdb96498fbe7d7e38
AlirezaCE <alirezace79@gmail.com>
2024-03-16 16:35:49 +0330

    add comment and post detail serializer

--- Gapoli_backend/post/models.py
+++ Gapoli_backend/post/models.py
@@ -18,6 +18,32 @@ class Comment(models.Model):
     created_by = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def since_created(self):
+            total_seconds = (timezone.now() - self.created_at).total_seconds()
+
+            years = int(total_seconds // 31536000)        
+
+            if years > 0:
+                return f"{years} year{'s' if years > 1 else ''}"
+            
+            else:
+                days = int(total_seconds // 86400)        
+                if days > 0:
+                    return f"{days} day{'s' if days > 1 else ''}"
+                
+                else:
+                    hours = int(total_seconds // 3600)
+                    if hours > 0:
+                        return f"{hours} hour{'s' if hours > 1 else ''}"
+                    
+                    else:
+                        minutes = int(total_seconds // 60)
+                        
+                        if minutes > 0:
+                            return f"{minutes} minute{'s' if minutes > 1 else ''}"
+                        else:
+                            return "just posted" 
+
 
 class PostAttachment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- Gapoli_backend/post/serializers.py
+++ Gapoli_backend/post/serializers.py
@@ -1,10 +1,28 @@
-from .models import Post
+from .models import Post, Comment
 from rest_framework import serializers
 from account.serializers import UserSerializer
+
 
 class PostSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
     
     class Meta:
         model = Post
-        fields = ['id','body','since_created','created_by','like_count']
+        fields = ['id','body','since_created','comment_count','created_by','like_count']
+     
+        
+class CommentSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id','text','since_created','created_by']
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
+    comment = CommentSerializer(read_only=True, many=True)
+    
+    class Meta:
+        model = Post
+        fields = ['id','body','since_created','created_by','like_count','comment']
